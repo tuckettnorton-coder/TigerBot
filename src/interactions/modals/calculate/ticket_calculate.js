16b442dd93a80da4fec6f89b3ce98e9c469f1223
tuckettnorton-coder <tuckett.norton@gmail.com>
2026-09-17 09:06:44 -0700

    Remove obsolete calculator modal handler

--- src/interactions/modals/calculate/ticket_calculate.js
+++
@@ -1,6 +0,0 @@
-import calculateModalHandler from '../../../handlers/calculateModals.js';
-
-export default {
-  name: 'ticket_calc_modal',
-  execute: calculateModalHandler.execute || calculateModalHandler,
-};
