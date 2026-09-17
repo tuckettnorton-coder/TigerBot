d9eae89fe1f3328bdb567aeb69f6fbac7e085b7e
tuckettnorton-coder <tuckett.norton@gmail.com>
2026-09-17 10:13:04 -0700

    Update spawner price destination channel

--- src/commands/Utility/spawner-update.js
+++ src/commands/Utility/spawner-update.js
@@ -6,7 +6,8 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const SPAWNER_PRICE_CHANNEL_ID = '1504949441650622575';
+// Public spawner price channel: 💵│spawner-prices
+const SPAWNER_PRICE_CHANNEL_ID = '1530000000000000000';
 const SPAWNER_UPDATE_ROLE_ID = '1509955955063001218';
 const DATA_FILE = path.join(__dirname, 'spawnerPrices.json');
 
