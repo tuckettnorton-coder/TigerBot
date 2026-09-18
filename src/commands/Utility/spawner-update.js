2af4622371fdf1d631c1d378bce2d08a6648c7f9
tuckettnorton-coder <tuckett.norton@gmail.com>
2026-09-17 21:33:37 -0700

    Fix spawner update ticket channel mention

--- src/commands/Utility/spawner-update.js
+++ src/commands/Utility/spawner-update.js
@@ -8,6 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const SPAWNER_PRICE_CHANNEL_ID = '1504948495948452001';
+const TICKET_CHANNEL_ID = '1504949441650622575';
 const SPAWNER_UPDATE_ROLE_ID = '1509955955063001218';
 const DATA_FILE = path.join(__dirname, 'spawnerPrices.json');
 const MESSAGE_FILE = path.join(__dirname, 'spawnerPriceMessage.json');
@@ -52,7 +53,27 @@ function saveMessageId(messageId) {
 
 export function formatPriceMessage(prices) {
   const { skeleton, creeper, irongolem } = prices;
-  return `${EMOJI_SKELETON} **Skeleton Spawners**\n**(You buy from us)**\n**3+ Buy:** ${skeleton.buy3}  **64+ Buy:** ${skeleton.buy64}\n**(You sell to us)**\n**3+ Sell:** ${skeleton.sell3}  **64+ Sell:** ${skeleton.sell64}\n\n${EMOJI_CREEPER} **Creeper Spawners**\n**(You buy from us)**\n**3+ Buy:** ${creeper.buy3}  **64+ Buy:** ${creeper.buy64}\n**(You sell to us)**\n**3+ Sell:** ${creeper.sell3}  **64+ Sell:** ${creeper.sell64}\n\n${EMOJI_IRONGOLEM} **Iron Golem Spawners**\n**(You buy from us)**\n**3+ Buy:** ${irongolem.buy3}  **64+ Buy:** ${irongolem.buy64}\n**(You sell to us)**\n**3+ Sell:** ${irongolem.sell3}  **64+ Sell:** ${irongolem.sell64}\n\n### __NOTE__- WE DON'T GO FIRST FOR BUYING/SELLING SPAWNERS MAKE A <#${SPAWNER_PRICE_CHANNEL_ID}> \n# MINIMUM 3+\n**ALL messages** regarding your ticket / spawners will be IN THE TICKET ONLY!! Scammers can SEE your ticket but not the messages inside, **dont get fooled by this**! <@&${SPAWNER_UPDATE_ROLE_ID}>`;
+  return `${EMOJI_SKELETON} **Skeleton Spawners**
+**(You buy from us)**
+**3+ Buy:** ${skeleton.buy3}  **64+ Buy:** ${skeleton.buy64}
+**(You sell to us)**
+**3+ Sell:** ${skeleton.sell3}  **64+ Sell:** ${skeleton.sell64}
+
+${EMOJI_CREEPER} **Creeper Spawners**
+**(You buy from us)**
+**3+ Buy:** ${creeper.buy3}  **64+ Buy:** ${creeper.buy64}
+**(You sell to us)**
+**3+ Sell:** ${creeper.sell3}  **64+ Sell:** ${creeper.sell64}
+
+${EMOJI_IRONGOLEM} **Iron Golem Spawners**
+**(You buy from us)**
+**3+ Buy:** ${irongolem.buy3}  **64+ Buy:** ${irongolem.buy64}
+**(You sell to us)**
+**3+ Sell:** ${irongolem.sell3}  **64+ Sell:** ${irongolem.sell64}
+
+### __NOTE__- WE DON'T GO FIRST FOR BUYING/SELLING SPAWNERS MAKE A <#${TICKET_CHANNEL_ID}> 
+# MINIMUM 3+
+**ALL messages** regarding your ticket / spawners will be IN THE TICKET ONLY!! Scammers can SEE your ticket but not the messages inside, **dont get fooled by this**! <@&${SPAWNER_UPDATE_ROLE_ID}>`;
 }
 
 export const data = new SlashCommandBuilder()
