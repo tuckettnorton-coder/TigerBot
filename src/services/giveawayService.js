cf62d3dd8974465f70ad467789f32e260dc26eca
tuckettnorton-coder <tuckett.norton@gmail.com>
2026-09-18 18:39:52 -0700

    Update giveaway winner claim message

--- src/services/giveawayService.js
+++ src/services/giveawayService.js
@@ -387,7 +387,7 @@ export async function checkGiveaways(client) {
         }
 
         if (winners.length > 0) {
-          const winnerAnnouncement = `🎉 Congratulations ${winnerMentions}! You won the **${giveaway.prize || 'giveaway'}**! Please contact <@${giveaway.hostId}> to claim your prize.`;
+          const winnerAnnouncement = `🎉 **Congratulations ${winnerMentions} on winning ${giveaway.prize || 'the giveaway'}!**\n\nMake sure to create a **Giveaway Claim Ticket** to claim your money in <#1504949441650622575>.`;
           const winnerPingMsg = await channel.send({ content: winnerAnnouncement });
           giveaway.winnerPingMessageId = winnerPingMsg.id;
           await markGiveawayEnded(client, giveawayId, giveaway);
