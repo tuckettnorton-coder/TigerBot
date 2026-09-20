2363c9260243c198efc985420566c8272b279c65
tuckettnorton-coder <tuckett.norton@gmail.com>
2026-09-19 18:06:21 -0700

    Fix Node 19 JSON import syntax

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ import { loadCommands, registerCommands as registerSlashCommands } from './handl
 import { runSafeTask, handleTaskError, ErrorCodes } from './utils/errorHandler.js';
 import { initializeMusic } from './services/music/riffySetup.js';
 import { shutdownMusic } from './services/music/playerHandler.js';
-import pkg from '../package.json' with { type: 'json' };
+import pkg from '../package.json' assert { type: 'json' };
 import { EXPECTED_SCHEMA_VERSION, EXPECTED_SCHEMA_LABEL } from './config/database/schemaVersion.js';
 
 class TitanBot extends Client {
