c95f3ae3c25afd653170f70cefd7cc92697587a8
tuckettnorton-coder <tuckett.norton@gmail.com>
2026-09-20 15:10:23 -0700

    Load persistent transcripts

--- src/interactions/buttons/downloadTranscript.js
+++ src/interactions/buttons/downloadTranscript.js
@@ -4,7 +4,7 @@ import { getTranscriptForMessage } from '../../services/transcriptStore.js';
 export default {
   name: 'download_transcript',
   async execute(interaction) {
-    const transcript = getTranscriptForMessage(interaction.message.id);
+    const transcript = await getTranscriptForMessage(interaction.message.id);
     if (!transcript) {
       await interaction.reply({
         content: 'This transcript is no longer available. Please ask a staff member to regenerate it.',
@@ -12,14 +12,9 @@ export default {
       });
       return;
     }
-
     await interaction.reply({
       content: '📄 Here is your transcript file.',
-      files: [
-        new AttachmentBuilder(transcript.buffer, {
-          name: transcript.fileName,
-        }),
-      ],
+      files: [new AttachmentBuilder(transcript.buffer, { name: transcript.fileName })],
       flags: MessageFlags.Ephemeral,
     });
   },
