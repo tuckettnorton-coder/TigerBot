33b3030524449c9f512abee2f1c16f9a727cefd7
tuckettnorton-coder <tuckett.norton@gmail.com>
2026-09-20 14:03:48 -0700

    Remove TigerBot application system and restore original ticket panel setup

--- src/events/tigerApplicationDMs.js
+++
@@ -1,3 +0,0 @@
-import { Events } from 'discord.js';
-import { getActive,recordAnswer,APP_CONFIG } from '../commands/Community/modules/tigerApplications.js';
-export default {name:Events.MessageCreate,async execute(message,client){try{if(message.author.bot||message.guild)return;for(const g of client.guilds.cache.values())for(const t of Object.keys(APP_CONFIG)){const a=await getActive(client,g.id,message.author.id,t);if(a&&a.status==='in_progress'){await recordAnswer(client,a,message.content);return}}}catch{}}};
