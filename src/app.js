bb6451cf4814f7a234f7527fbe087287ba54b3d2
tuckettnorton-coder <tuckett.norton@gmail.com>
2026-09-19 20:37:45 -0700

    Remove giveaway startup and scheduled tasks

--- src/app.js
+++ src/app.js
@@ -10,8 +10,6 @@ import { getGuildConfig } from './services/config/guildConfig.js';
 import { getServerCounters, saveServerCounters, updateCounter } from './services/serverstatsService.js';
 import { logger, startupLog, shutdownLog } from './utils/logger.js';
 import { checkBirthdays } from './services/birthdayService.js';
-import { checkGiveaways } from './services/giveawayService.js';
-import { checkAutoGiveaways } from './services/autoGiveawayService.js';
 import { loadCommands, registerCommands as registerSlashCommands } from './handlers/loaders/commandLoader.js';
 import { runSafeTask, handleTaskError, ErrorCodes } from './utils/errorHandler.js';
 import { initializeMusic } from './services/music/riffySetup.js';
