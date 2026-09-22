9a53d3af2e5d2905b4becacc4ef899b617a53ec7
tuckettnorton-coder <tuckett.norton@gmail.com>
2026-09-22 15:07:49 -0700

    Persist paid ad update values for modal reopening

--- src/commands/Utility/paid-ad-update.js
+++ src/commands/Utility/paid-ad-update.js
@@ -1,5 +1,6 @@
 import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
 import { loadPaidAdPrices } from '../../utils/paidAdPricing.js';
+import { getLatestUpdateData } from '../../utils/updateState.js';
 
 export const PAID_AD_UPDATE_CHANNEL_ID = '1525901034747330693';
 
@@ -8,9 +9,10 @@ export const data = new SlashCommandBuilder()
   .setDescription('Update Paid Advertisement prices and repost the price list')
   .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild);
 
-export async function execute(interaction) {
+export async function execute(interaction, guildConfig, client) {
   const { buildPaidAdUpdateModal } = await import('../../interactions/modals/paid_ad_update_modal.js');
-  await interaction.showModal(buildPaidAdUpdateModal(loadPaidAdPrices(), 1));
+  const saved = await getLatestUpdateData(client, interaction.guildId, 'paidAdPrices', null);
+  await interaction.showModal(buildPaidAdUpdateModal(saved || loadPaidAdPrices(), 1));
 }
 
 export default { data, execute };
